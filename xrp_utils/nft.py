d5eb6b0497f20d4ffc473f254e3f9eb8fea069e7
Obiajulu <47371105+ObiajuluM@users.noreply.github.com>
2023-10-23 07:31:42 +0100

    Update nft.py

--- xrp_utils/nft.py
+++ xrp_utils/nft.py
@@ -1,14 +1,21 @@
 from typing import Union
 from xrpl.asyncio.clients import AsyncJsonRpcClient
 import requests
-from xrpl.asyncio.clients.client import Client
 from xrpl.models import (AccountObjects, IssuedCurrencyAmount, NFTBuyOffers,
                          NFTokenAcceptOffer, NFTokenCancelOffer,
                          NFTokenCreateOffer, NFTokenCreateOfferFlag,
                          NFTSellOffers)
 from xrpl.utils import drops_to_xrp, ripple_time_to_datetime, xrp_to_drops
 
-from Misc import mm
+from xrpl.models import (AccountSet, AccountSetFlag, IssuedCurrencyAmount,
+                         NFTokenBurn, NFTokenMint, NFTokenMintFlag, Payment,
+                         TrustSet, Transaction, AccountSetAsfFlag)
+from xrpl.clients.json_rpc_client import JsonRpcClient
+from xrpl.transaction.main  import autofill_and_sign, sign_and_submit
+from xrpl.wallet import Wallet
+
+from Misc import (mm, nft_fee_to_xrp_format,
+                  transfer_fee_to_xrp_format, validate_symbol_to_hex)
 from x_constants import M_SOURCE_TAG
 
 """nft handler"""
@@ -60,3 +67,26 @@ def accept_nft_offer(sender_addr: str, sell_offer_id: str = None, buy_offer_id: 
         nftoken_sell_offer=sell_offer_id,
         nftoken_broker_fee=amount, fee=fee, memos=mm(), source_tag=M_SOURCE_TAG)
     return txn.to_xrpl() 
+
+
+
+def mint_nft(issuer_addr: str, taxon: int, is_transferable: bool, only_xrp: bool, issuer_burn: bool, transfer_fee: float = None, uri: str = None, fee: str = None) -> dict:
+    """mint nft"""
+    flag = []
+    if is_transferable:
+        flag.append(NFTokenMintFlag.TF_TRANSFERABLE) # nft can be transferred
+    if only_xrp:
+        flag.append(NFTokenMintFlag.TF_ONLY_XRP) # nft may be traded for xrp only
+    if issuer_burn:
+        flag.append(NFTokenMintFlag.TF_BURNABLE) # If set, indicates that the minted token may be burned by the issuer even if the issuer does not currently hold the token.
+    txn = NFTokenMint(
+        account=issuer_addr,
+        nftoken_taxon=taxon,
+        uri=validate_symbol_to_hex(uri), flags=flag, transfer_fee=nft_fee_to_xrp_format(transfer_fee), fee=fee, memos=mm(), source_tag=M_SOURCE_TAG)
+    return txn.to_xrpl()
+
+def burn_nft(sender_addr: str, nftoken_id: str, holder: str = None, fee: str = None) -> dict:
+    """burn an nft, specify the holder if the token is not in your wallet, only issuer and holder can call"""
+    txn = NFTokenBurn(account=sender_addr, nftoken_id=nftoken_id, owner=holder, fee=fee, memos=mm(), source_tag=M_SOURCE_TAG)
+    return txn.to_xrpl()
+
